26aabb03524fc63a108dc5b38b8c85b80851bac5
Jason Lei <jasonlei@Jasons-MacBook-Pro-2.local>
2021-06-29 00:15:16 -0700

    final

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,14 +1,38 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// call table
 var tbody = d3.select("tbody");
+var visual = d3.select("table");
 
-data.forEach((ufos) => {
-    var row = tbody.append("tr");
-    Object.entries(ufos).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
+// build table
+function inputTable(ufoData) {
+    ufoData.forEach((ufo) => {
+        var row = tbody.append("tr");
+        Object.entries(ufo).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
     });
-  });
-  
+};
+
+inputTable(tableData);
+console.log(tableData);
+
+// create filtered table
+
+var filterBtn = d3.select("#filter-btn");
+var inputDate = d3.select("#datetime");
+
+filterBtn.on("click", function() {
+    d3.event.preventDefault(); 
+    tbody.html("");
+    
+    var inputValue = inputDate.property("value");
+    console.log(inputValue);
+
+    var filterTable = tableData.filter(tableData => tableData.datetime === inputValue);
+    console.log(filterTable);
+
+    inputTable(filterTable);
+});
